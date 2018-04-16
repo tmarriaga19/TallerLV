d58f7264ff8ef3d469b4f0664bb58b6d81131802
Tommy <tmarriaga@gmail.com>
2018-04-16 16:45:55 -0500

    Validaciones volumenes terminadas

--- app/src/main/java/com/tmarriaga/tallerlv/cubo.java
+++ app/src/main/java/com/tmarriaga/tallerlv/cubo.java
@@ -1,13 +1,70 @@
 package com.tmarriaga.tallerlv;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class cubo extends AppCompatActivity {
+
+    private TextView res;
+    private EditText n1;
+    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cubo);
+
+        n1 = (EditText)findViewById(R.id.txtAreaUnLado);
+        res = (TextView)findViewById(R.id.txtResultado);
+        resources = this.getResources();
+
+    }
+
+    public boolean validar (){
+
+        if (n1.getText().toString().isEmpty()) {
+            n1.setError(resources.getString(R.string.mensaje_de_error_uno));
+            return false;
+        }
+        return true;
+    }
+
+    //Estructura para llamar a un botón
+    public void calcular (View v){
+
+        int num1, resultado=0;
+        res.setText("");
+
+        if (validar()){
+
+            num1 = Integer.parseInt(n1.getText().toString()) ;
+            resultado = (num1 * 6);
+
+            String ope,dat,result;
+            ope = resources.getString(R.string.area_del_cubo);
+            dat = "Area un lado: "+num1;
+            result = ""+resultado;
+
+            operacion o = new operacion(ope,dat,result);
+            o.guardar();
+            Toast.makeText(this,resources.getString(R.string.operacion_registrada), Toast.LENGTH_SHORT).show();
+
+            res.setText("\n\n"+"Resultado "+"\n"+ "Area: "+ resultado);
+        }
+
+    }
+
+    //Estructura para llamar a un botón
+    public void borrar (View v){
+
+        res.setText("");
+        n1.setText("");
+        n1.requestFocus();
+
     }
 }
--- app/src/main/java/com/tmarriaga/tallerlv/operaciones.java
+++ app/src/main/java/com/tmarriaga/tallerlv/operaciones.java
@@ -2,12 +2,44 @@ package com.tmarriaga.tallerlv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class operaciones extends AppCompatActivity {
+
+    private TableLayout tabla;
+    private ArrayList<operacion> operaciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operaciones);
+        tabla = (TableLayout)findViewById(R.id.tabla);
+        operaciones=Datos.Obtener();
+
+        for (int i = 0; i < operaciones.size(); i++){
+            TableRow fila = new TableRow(this);
+            TextView c1 = new TextView(this);
+            TextView c2 = new TextView(this);
+            TextView c3 = new TextView(this);
+            TextView c4 = new TextView(this);
+
+            int no=i+1;
+            c1.setText(""+no);
+            c2.setText(""+operaciones.get(i).getOperacion());
+            c3.setText(""+operaciones.get(i).getDatos());
+            c4.setText(""+operaciones.get(i).getResultado());
+
+            fila.addView(c1);
+            fila.addView(c2);
+            fila.addView(c3);
+            fila.addView(c4);
+
+            tabla.addView(fila);
+        }
+
     }
 }
