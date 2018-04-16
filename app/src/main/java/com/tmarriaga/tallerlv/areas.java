ed73792d8f96ec22f59b1f8006c7046bf8f71936
Tommy Marriaga <tmarriaga@gmail.com>
2018-04-15 22:57:30 -0500

    Archivos iniciales

---
+++ app/src/main/java/com/tmarriaga/tallerlv/areas.java
@@ -0,0 +1,13 @@
+package com.tmarriaga.tallerlv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class areas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_areas);
+    }
+}
